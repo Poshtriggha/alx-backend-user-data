69964e3a2d5d450267f07b3f7c9b7d492b3459cb
posh <mrposhboo@gmail.com>
2024-05-30 17:09:07 +0100

    dbbase

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """  has two required string arguments: email and
+        hashed_password, and returns a User """
+
+        user = User(email=email, hashed_password=hashed_password)
+        session = self._session
+        session.add(user)
+        session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ returns the first row found in the users table as filtered
+        by the method’s input arguments """
+        session = self._session
+        try:
+            user = session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except IndentationError as e:
+            raise IndentationError from e
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ takes as argument a required user_id integer and arbitrary
+        keyword arguments, and returns None """
+        session = self._session
+
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        session.commit()
+        return None
